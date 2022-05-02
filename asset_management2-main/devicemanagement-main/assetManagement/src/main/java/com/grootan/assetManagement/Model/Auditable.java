3d0ce79561db3bbf4c7e7053d6506811dc22dfa2
sheik <sheikabdullah.m@grootan.com>
2022-05-02 17:26:06 +0530

    login index color changed

--- asset_management2-main/devicemanagement-main/assetManagement/src/main/java/com/grootan/assetManagement/Model/Auditable.java
+++ asset_management2-main/devicemanagement-main/assetManagement/src/main/java/com/grootan/assetManagement/Model/Auditable.java
@@ -8,15 +8,14 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
-
 import java.util.Date;
-
-import static javax.persistence.TemporalType.TIMESTAMP;
+import static javax.persistence.TemporalType.*;
 
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class Auditable<U> {
+public abstract class Auditable<U>
+{
     @CreatedBy
     protected U createdBy;
     @CreatedDate
